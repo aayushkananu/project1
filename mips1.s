096d1cd0826c426449d248040790751c84bd011e
aayushkananu <aayushkab.2003@gmail.com>
2024-02-06 15:33:06 -0500

    second commit

--- mips1.s
+++ mips1.s
@@ -1,8 +1,11 @@
 .data
-space: .asciiz "\n"
+space: .asciiz "\n" 
 input: .asciiz "Enter the string: "
-input_string: .space 1000
+input_string: .space 1000 #space for 1000 characters has been allocated, and the main program has been started.
 
 .text
 .globl main
 	main: 
+li $v0, 8 #taking user input for strings
+la $a0, input_string 
+
